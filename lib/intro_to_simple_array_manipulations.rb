ab4c8ad1292811651dff6f999c1cd6808d8fb7fd
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-10 17:04:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,11 +15,11 @@ def pop_with_args (array)
 end
 
 def using_shift (array)
-  array.shift(0)
+  array.shift
 end
 
 def shift_with_args (array)
-  
+  array.shift(2)
 end
 
 def using_concat (array1, array2)
